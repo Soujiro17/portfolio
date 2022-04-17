421d6e1344eb0711fb6cf85bb6f24567a1e5c046
Soujiro17 <vicente_tomi@hotmail.com>
2022-04-17 00:03:30 -0400

    arrow up icon

--- src/components/GoUp/index.jsx
+++ src/components/GoUp/index.jsx
@@ -1,20 +1,30 @@
 import styled from 'styled-components'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { accent, white, whiteHover } from '../../constants/colors'
+import ArrowUp from '../Icons/ArrowUp'
 
 const GoUpContainer = styled.div`
     position: fixed;
     right: 2rem;
     bottom: 2rem;
     height: 4.5rem;
+    min-height: 4.5rem;
     width: 4.5rem;
+    min-width: 4.5rem;
     background-color: ${accent};
     border-radius: 50%;
     border: 1px solid ${white};
     z-index: 1000;
     color: ${white};
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     transition: .2s all;
+    svg{
+        height: 50%;
+        width: 50%;
+    }
     &:hover{
         background-color: ${whiteHover};
     }
@@ -39,7 +49,9 @@ const GoUp = () => {
     window.addEventListener("scroll", toggleVisible)
 
   return (
-    <GoUpContainer onClick={scrollToTop} style = {goUpStyle}>GoUp</GoUpContainer>
+    <GoUpContainer onClick={scrollToTop} style = {goUpStyle}>
+        <ArrowUp />
+    </GoUpContainer>
   )
 }
 
